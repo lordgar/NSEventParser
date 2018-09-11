3a9fb0fea648c90d951a560608e4d950ae1f425f
Garrett Olson <lordgar@gmail.com>
2018-09-10 23:23:49 -0700

    added post

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000
 
+app.use(express.json());
+
 app.get('/', (req,res) =>{
     res.send('NSEventParser')
+})
+
+app.post('/event/:eid', (req, res) => {
+    console.log(`NS Event: ${req.params.eid}`)
+    console.log(`body: ${req.body}`)
 })
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
