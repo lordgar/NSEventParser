4c054058ad5bac94eabe2cae91321ae7eb41fb64
Garrett Olson <lordgar@gmail.com>
2018-09-11 18:48:53 -0700

    JSON parse

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.get('/', (req,res) =>{
 
 app.post('/event/:eid', (req, res) => {
     console.log(`NS Event: ${req.params.eid}`);
-    console.log(req.body);
-    res.status(200).send(req.body);
+    const bodystring = JSON.stringify(req.body)
+    console.log(bodystring);
+    res.status(200).send(bodystring);
 })
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
