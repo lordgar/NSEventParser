1357158b0ff2791ec57c3be06b81eb6088eb2595
Garrett Olson <lordgar@gmail.com>
2018-09-14 13:04:14 -0700

    initial Pushover

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var Pushover = require('pushover-notifications');
+var Push = require('pushover-notifications');
 const app = express();
 const PORT = process.env.PORT || 5000
 const USER = process.env.PUSHOVER_USER
