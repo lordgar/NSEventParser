53a329ecfa79264a3516fc88a7509829feea9abd
Garrett Olson <lordgar@gmail.com>
2018-09-14 23:43:47 -0700

    initial Pushover

--- index.js
+++ index.js
@@ -22,32 +22,61 @@ app.post('/event/:eid', (req, res) => {
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 function parseForPushover(inParams, inQuery) {
-    var eventTitle = inQuery.Value1.trim()
+    var v1 = inQuery.Value1.trim();
+    var v2 = inQuery.Value2.trim();
+    var eventTitle = v1 + ' (' + inParams.eid + ')';
+    var eventSound = 'pushover';
+    var eventMessage = v2;
+    var eventPriority = -2;
+    var sendMessage = true;
     var p = new Push( {
         user: USER,
         token: TOKEN,
-        // httpOptions: {
-        //   proxy: process.env['http_proxy'],
-        //},
-        // onerror: function(error) {},
-        // update_sounds: true // update the list of sounds every day - will
-        // prevent app from exiting.
-      })
+    })
+    
+    switch (inParams.eid) {
+        case 'ns-event':
+            sendMessage = false;
+            break;
+        case 'ns-info':
+            if(v1 === 'CGM Error Code')
+            {
+                eventPriority = 0;
+                eventSound = 'alien'; 
+            }
+            sendMessage = true;
+            break;
+        case 'ns-warning':
+            eventSound = 'gamelan';
+            eventPriority = -1;
+            sendMessage = true;
+            break;
+        case 'ns-urgent':
+            eventSound = 'gamelan';
+            eventPriority = -1;
+            sendMessage = true;
+            break;    
+        default:
+            break;
+    }
+      
+    if(sendMessage){
       var msg = {
-        // These values correspond to the parameters detailed on https://pushover.net/api
-        // 'message' is required. All other values are optional.
-        message: 'put message data here',	// required
-        title: eventTitle,
-        sound: 'magic',
-        priority: 1
-      }
-       
-      p.send( msg, function( err, result ) {
-        if ( err ) {
-          throw err
-        }
-       
-        console.log( result )
-      })
+            message: eventMessage,	// required
+            title: eventTitle,
+            sound: eventSound,
+            priority: eventPriority
+          }
+           
+          p.send( msg, function( err, result ) {
+            if ( err ) {
+              throw err
+            }
+           
+            console.log( result )
+          })    
+    } else {
+        console.log('message not sent')
+    }
 
 }
